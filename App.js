85ae9592ff8e5ecac9cd3baf4a6e9fc7d6176fe9
Priti <priti.tripathi@infoprolearning.com>
2025-06-28 01:01:54 +0530

    episode 01

--- App.js
+++ App.js
@@ -1,23 +1,19 @@
+// const heading = React.createElement("h1", { id: "heading" }, "React");
+
 const parent = React.createElement(
-    "div",
-    {id:"parent"}, 
-    [
-    React.createElement("div", {id:"child"}, 
-        [React.createElement("h1", {}, "I am h1 tag"),
-            React.createElement("h3", {}, "I am h3 tag")
-        ]
-    ),
-    React.createElement("div", {id:"child2"}, 
-        [React.createElement("h1", {}, "I am h1 tag"),
-            React.createElement("h3", {}, "I am h3 tag")
-        ]
-    )
+  "div",
+  { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am H1 tag"),
+    React.createElement("h2", {}, "I am H2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am H1 tag"),
+    React.createElement("h2", {}, "I am H2 tag"),
+  ])
 ]
-)
+);
 
-console.log(parent);
-
-// const heading = React.createElement("h1", { id: "heading" }, "Hello World from React!");
-// console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(parent);
